7d7f7a674edd03aecdbcbdc65e42398ab1dc67fd
Knight Wing <ankithg03@gmail.com>
2019-08-23 18:27:45 +0530

    first commit

--- registration.php
+++ registration.php
@@ -1,7 +1,7 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'NewNamespaceCreated_NewModule',
+    'newnamespacecreated_newmodule',
     __DIR__
 );
 ?>
